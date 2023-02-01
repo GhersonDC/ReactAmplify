77f38911667938558c9d2fc75932a56c288f5b4e
Gherson David <ing.gherson@yahoo.com>
2023-02-01 11:09:51 -0800

    fisrt commit and updates only for tasks basics

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { API, graphqlOperation } from "aws-amplify";
+import { createTask } from "./graphql/mutations";
+import { listTasks } from "./graphql/queries";
+import React ,{ useEffect, useState } from "react";
 
 function App() {
+  
+  const [task, setTask] = useState({
+    name: "",
+    description: "",
+  });
+
+  const [tasks, setTasks] = useState({
+    
+  });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log(task);
+    const result = await API.graphql(
+      graphqlOperation(createTask, { input: task })
+    );
+    console.log(result);
+  };
+  useEffect(() => {
+    async function loadTasks() {
+      const result = await API.graphql(graphqlOperation(listTasks));
+      setTasks(result.data.listTasks.items);
+    }
+    loadTasks();
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <form onSubmit={handleSubmit}>
+        <input
+          name="name"
+          placeholder="name"
+          onChange={(e) => setTask({ ...task, name: e.target.value })}
+        ></input>
+        <textarea
+          name="description"
+          placeholder="description"
+          onChange={(e) => setTask({ ...task, description: e.target.value })}
+        ></textarea>
+        <button>Submit</button>
+      </form>
+      {JSON.stringify(tasks)}
+    </>
   );
 }
 
